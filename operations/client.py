21210c512543e5369726987594202d41d76c5e10
magoarcano <magoarcano@gmail.com>
2019-01-27 17:52:25 +0300

    Managing array of N processes working in parallel

--- operations/client.py
+++ operations/client.py
@@ -12,7 +12,9 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 45454        # The port used by the server
 BUFFER_SIZE = 8192
 
+import timeit
 if __name__ == '__main__':
+    tic=timeit.default_timer()
     s = socket.socket()
     s.connect((HOST, PORT))
     fin = open ("head.txt", "r") # head.txt small temporary file for development
@@ -25,14 +27,12 @@ if __name__ == '__main__':
     
     results = s.recv(BUFFER_SIZE)
     while (results):
-#         logging.info("Results: %s" % results)
         fout.write(results)
-        print results
-        print "-------"
-#         if len(results) < BUFFER_SIZE:
-#             break
         results = s.recv(BUFFER_SIZE)
     logging.info("Saving in file results.txt")
     fout.close()    
     s.close()
+    toc=timeit.default_timer()
+    dif = toc - tic
+    print "TIEMPO: " + str(dif)
     
--- operations/mpserver.py
+++ operations/mpserver.py
@@ -17,10 +17,15 @@ BUFFER_SIZE = 8192
 PROCESSORS = cpu_count()
 
 class OperationProcess(Process):
-    def __init__(self, operations, conn):
+    def __init__(self, operations, conn, segment):
+        """
+        :param operations: list of arithmetic operations
+        :param conn: pipe connection
+        """
         super(OperationProcess, self).__init__()
         self.operations = operations
         self.conn = conn
+        self.segment = segment
         
     def run(self):
         result_list = [self.operate(op) for op in operations if op != '' ]
@@ -45,27 +50,60 @@ if __name__ == '__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST,PORT))
     server_socket.listen(10) # Accept until 10 incoming connections. Parameter optional in Python 3
-    children = []
+    children = [None] * PROCESSORS
 #     while True:
     client_socket, address = server_socket.accept()
     last =  ''
     data = client_socket.recv(BUFFER_SIZE)
+    
+    i = 0
     while (data):
+        """
+        TODO: START PRIMERO TODOS JUNTOS
+        LUEGO JOINS DE A UNO. QUIZA AL RECIBIR INFO DESDE LAS PIPES 
+        """
+        i = i % PROCESSORS # 
+        process = children[i]
+        if process:
+            p, parent_conn, child_conn = process
+            results = parent_conn.recv()
+            p.join()
+            client_socket.sendall(results)
+            # close pipes
+            parent_conn.close()
+            child_conn.close()
+            children[i] = None
+        
         operations = data.split("\n") # Better than splitlines to detect cropped operations
         operations[0] = last + operations[0] # concatenates with last operations of previous segment
         last = operations.pop()
-    
+        
         parent_conn, child_conn = Pipe()
-        p = OperationProcess(operations, child_conn)
-        children.append({p.pid: [parent_conn, child_conn]})
+        p = OperationProcess(operations, child_conn, i)
+        children[i] = [p, parent_conn, child_conn]
         p.start()
-        results = parent_conn.recv()
-        p.join()
-        client_socket.sendall(results)
-        if len(data) < BUFFER_SIZE:
+        i += 1
+        if len(data) < BUFFER_SIZE:# and children == [None] * PROCESSORS:
             break
         data = client_socket.recv(BUFFER_SIZE)
+        
+    empty = [None] * PROCESSORS
+    while children != empty:
+        i = i % PROCESSORS # 
+        process = children[i]
+        if process:
+            p, parent_conn, child_conn = process
+            results = parent_conn.recv()
+            p.join()
+            client_socket.sendall(results)
+            # close pipes
+            parent_conn.close()
+            child_conn.close()
+            children[i] = None
+        i += 1
+    
     client_socket.close()
-    #endwhile
     server_socket.close()
+    #endwhile
+    
 
